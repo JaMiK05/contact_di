7697d591ec4cd723fc291e4f6dd27751f98e5b47
JaMiK <jamshidbekikromov0@gmail.com>
2023-05-23 14:04:26 +0500

    snackbar

--- app/src/main/java/uz/gita/contact_di/presentation/screen/MainActivity.kt
+++ app/src/main/java/uz/gita/contact_di/presentation/screen/MainActivity.kt
@@ -3,7 +3,9 @@ package uz.gita.contact_di.presentation.screen
 import android.os.Bundle
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import by.kirich1409.viewbindingdelegate.viewBinding
+import com.google.android.material.snackbar.Snackbar
 import dagger.hilt.android.AndroidEntryPoint
 import uz.gita.contact_di.R
 import uz.gita.contact_di.databinding.ActivityMainBinding
@@ -17,6 +19,13 @@ class MainActivity : AppCompatActivity(R.layout.activity_main) {
         super.onCreate(savedInstanceState)
 
         Toast.makeText(this, "vuiguiguigigghgyghyghygghyg", Toast.LENGTH_SHORT).show()
+
+        Snackbar.make(binding.root,
+            "Check if the fields are not empty!!!",
+            Snackbar.LENGTH_SHORT)
+            .setBackgroundTint(ContextCompat.getColor(this,
+                R.color.teal_700))
+            .show()
 
     }
 }
